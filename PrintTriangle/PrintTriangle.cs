5b638cb5924b8ac331a7e2761157c02865f24a24
PoojaRajbhar3003 <136968646+PoojaRajbhar3003@users.noreply.github.com>
2023-07-01 13:46:10 +0530

    Add files via upload

---
+++ PrintTriangle/PrintTriangle.cs
@@ -0,0 +1,32 @@
+﻿namespace PrintTriangle{
+
+    class PrintTriangle{
+       
+        static void Main(string[] args){
+        
+            int height, i,j,lft,rt;
+
+            //taking height from user
+            height = Convert.ToInt32(Console.ReadLine());
+
+            rt= lft = height;
+        
+            for(i=1;i<=height;i++)
+            {
+                for(j=1;j<(height*2);j++){
+                    if(j<lft||j>rt){
+                        Console.Write(" ");
+                    }
+                    else{
+                        Console.Write("*");
+                    }
+                }
+                lft--;
+                rt++;
+                Console.WriteLine("\n");
+            }    
+
+        }
+    
+    }
+}
